47b7119fc29976c1d65b1a95d4096d2e4d7d454d
Richard <40587587+Quetives@users.noreply.github.com>
2020-03-13 11:47:51 -0700

    Add project files.

---
+++ Assignment 231/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_231
+{
+    class Employee
+    {
+        public string empName { get; set; }
+        public int empId { get; set; }
+        public Employee(string name) : this(name, 0) { }
+        public Employee(string name, int Id)
+        {
+            empName = name;
+            empId = Id;
+        }
+    }
+}
---
+++ Assignment 231/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_231
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string companyName = "Acme Industries";
+            var greeting = "Welcome to "+ companyName;
+            Console.WriteLine(greeting);
+            Employee richard = new Employee("Richard");
+            Console.WriteLine("{0}, You are employee number:{1}", richard.empName, richard.empId);
+            Console.ReadLine();
+
+
+        }
+    }
+}
